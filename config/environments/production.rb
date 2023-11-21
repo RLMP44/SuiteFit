6b36c662117d7a497129e69647f84c4a4f6aad6a
Rina Iwamine <leddihome@gmail.com>
2023-11-21 17:24:28 +0900

    fixed pundit errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  # Raise error when a before_action's only/except options reference missing actions
+  config.action_controller.raise_on_missing_callback_actions = false
 end
