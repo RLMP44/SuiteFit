c91cf25bca723953ebe950bc7b4975b390853f3b
Rina Iwamine <leddihome@gmail.com>
2023-11-22 18:03:43 +0900

    creating an apartment wip

--- app/javascript/controllers/apartment_canvas_controller.js
+++ app/javascript/controllers/apartment_canvas_controller.js
@@ -3,23 +3,36 @@ import { fabric } from 'fabric'
 
 // Connects to data-controller="apartment-canvas"
 export default class extends Controller {
-  static targets = ["canvas"]
-  static values = [{'floorplanUrl':String}]
+  static targets = ["canvas", "drawButton", 'saveButton']
+  static values = {'floorplanUrl': String}
   connect() {
     // create new canvas
-    const canvas = new fabric.Canvas('new-canvas', {
+    // canvas should scale to the picture size?
+    const canvas = this.canvas = new fabric.Canvas('new-canvas', {
       width: 800,
       height: 1000
     })
 
     // set a floorplan on the canvas
-    console.log(this.floorplanUrlValue)
-    fabric.Image.fromURL(this.floorplanUrlValue, function(floorplan) {
-      canvas.add(floorplan);
-    });
-    // const floorPlan = document.createElement("img")
-    // floorPlan.scr = this.floorplanUrlValue
-    // const floorPlanInstance = new fabric.Image(floorPlan);
-    // canvas.add(floorPlanInstance)
+    fabric.Image.fromURL(this.floorplanUrlValue, function(img) {
+      img.scaleToWidth(canvas.getWidth());
+      // canvas.add(img);
+      canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas))
+    })
+  }
+
+  // trace a floorplan on the canvas
+  drawFloorplan(event) {
+    this.canvas.isDrawingMode = true
+    event.currentTarget.classList.add('d-none')
+    this.saveButtonTarget.classList.remove('d-none')
+  }
+
+  // save the canvas to JSON/serialize
+  saveFloorplan(event) {
+    this.canvas.isDrawingMode = false
+    event.currentTarget.classList.add('d-none')
+    this.drawButtonTarget.classList.remove('d-none')
+    // JSON.stringify(canvas)
   }
 }
