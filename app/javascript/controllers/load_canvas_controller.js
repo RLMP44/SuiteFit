baacf14008d997fae2013dfb6fb6338debdc7241
Rachael Momber <rlmomber@gmail.com>
2023-11-22 22:43:40 +0900

    added scaling and lockScaling capabilities, and add element to page (but not working yet)

--- app/javascript/controllers/load_canvas_controller.js
+++ app/javascript/controllers/load_canvas_controller.js
@@ -1,24 +1,46 @@
 import { Controller } from "@hotwired/stimulus"
 import { fabric } from "fabric"
+import { Canvas2dFilterBackend } from "fabric/fabric-impl"
 
 // Connects to data-controller="load-canvas"
 export default class extends Controller {
-  static targets = ["canvas"]
+  static targets = ["canvas"] //"my-image"
   static values = {json: String}
 
   connect() {
     console.log("hi")
-    // fake json data
-    const serializedjson = this.jsonValue
+    // retrieve apartment floor plan as serialized json
+    const serializedJson = this.jsonValue
+    // create empty canvas
     const canvas = new fabric.Canvas("new-canvas", {
       height: 500,
       width: 600,
     })
-    const renderedCanvas = this.loadCanvas(canvas, serializedjson)
+    // call function to load canvas
+    const renderedCanvas = this.loadCanvas(canvas, serializedJson)
     console.log(renderedCanvas)
+    // get image/icon element to add to canvas
+    const imgElement = this.dataTarget('my-image')
+    // find on-canvas length of selected door, scale to size using standard 80 cm door
+    const ratio = this.selectedObject.length / 80
+    // create instance of user item on canvas and scale with ratio
+    const imgInstance = new fabric.Image(imgElement, {
+      // pass in data, note that length doesn't exist in fabric so it becomes height
+      height: this.dataTarget.length * ratio,
+      width: this.dataTarget.width * ratio,
+      top: 20,
+      left: 20,
+      lockScalingX: true,
+      lockScalingY: true,
+    });
+  canvas.add(imgInstance);
+  canvas.renderAll();
   }
 
-  loadCanvas = (canvas, serializedjson) => {
-    return canvas.loadFromJSON(serializedjson, canvas.renderAll.bind(canvas));
+  // create function to deserialize json and render apartment floor plan
+  loadCanvas = (canvas, serializedJson) => {
+    return canvas.loadFromJSON(serializedJson, canvas.renderAll.bind(canvas));
   }
 }
+
+// add clear items button
