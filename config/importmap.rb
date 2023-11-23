10674cb7cdac177a43bbd688b00d4b1cc38f5984
Victor Shinji Matsudo <victormatsudo.vsm@gmail.com>
2023-11-23 12:07:21 +0900

    Fix edit button for user items

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "fabric", to: "https://ga.jspm.io/npm:fabric@5.3.0/dist/fabric.js"
+pin "sweetalert2", to: "https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.all.min.js", preload: true
