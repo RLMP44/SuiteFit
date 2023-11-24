8559394a7d497aca07b5ba714dea531acce5c68e
Rachael Momber <rlmomber@gmail.com>
2023-11-24 13:10:30 +0900

    fixed comments

--- app/javascript/controllers/load_canvas_controller.js
+++ app/javascript/controllers/load_canvas_controller.js
@@ -36,6 +36,15 @@ export default class extends Controller {
 
   // method to add clicked item to canvas
   add(event) {
+    //   // get image/icon element to add to canvas
+      //   const imgElement = this.dataTarget('my-image')
+      //   // find on-canvas length of selected door, scale to size using standard 80 cm door
+      //   const ratio = this.selectedObject.length / 80
+      //   // create instance of user item on canvas and scale with ratio
+      //   const imgInstance = new fabric.Image(imgElement, {
+        //     // pass in data, note that length doesn't exist in fabric so it becomes height
+        //     height: this.dataTarget.length * ratio,
+        //     width: this.dataTarget.width * ratio,
     const rect = new fabric.Rect({
         height: parseInt(event.currentTarget.dataset.length),
         width: parseInt(event.currentTarget.dataset.width),
@@ -45,8 +54,8 @@ export default class extends Controller {
         hasBorders: false,
         transparentCorners: true,
         cornerSize: 5,
-    })
-    this.canvas.add(rect)
+      })
+      this.canvas.add(rect)
   }
 
   // method to clear the canvas
@@ -55,9 +64,8 @@ export default class extends Controller {
     this.canvas.clear()
   }
 
+  // method to save arrangement
   save(event) {
-    console.log(event.currentTarget.dataset.bookmark)
-    console.log(event.currentTarget.dataset.arrangement)
     fetch(`/bookmarks/${event.currentTarget.dataset.bookmark}`, {
       method: "PATCH",
       body: JSON.stringify(this.canvas),
@@ -65,17 +73,6 @@ export default class extends Controller {
         "X-CSRF-Token": this.getMetaValue("csrf-token")
       }
     }).then(console.log("Success"))
-    // console.log(event.currentTarget.dataset.arrangement)
-    // stringified_file = JSON.stringify(this.canvas)
-    // console.log("stringified_file below")
-    // console.log(stringified_file)
-    // console.log("dataset below")
-    // console.log(event.currentTarget.dataset.arrangement)
-    // console.log("current canvas below")
-    // console.log(JSON.stringify(this.canvas))
-    // event.currentTarget.dataset.arrangement = JSON.stringify(this.canvas)
-    // console.log("saved?? dataset below")
-    // console.log(event.currentTarget.dataset.arrangement)
   }
 
   // function to deserialize json and render apartment floor plan or bookmark arrangement
@@ -83,56 +80,13 @@ export default class extends Controller {
     return canvas.loadFromJSON(json, canvas.renderAll.bind(canvas));
   }
 
+  // method to be used in save function
   getMetaValue(name) {
     const element = document.head.querySelector(`meta[name="${name}"]`)
     return element.getAttribute("content")
   }
 }
-//   // get image/icon element to add to canvas
-  //   const imgElement = this.dataTarget('my-image')
-  //   // find on-canvas length of selected door, scale to size using standard 80 cm door
-  //   const ratio = this.selectedObject.length / 80
-  //   // create instance of user item on canvas and scale with ratio
-  //   const imgInstance = new fabric.Image(imgElement, {
-    //     // pass in data, note that length doesn't exist in fabric so it becomes height
-    //     height: this.dataTarget.length * ratio,
-    //     width: this.dataTarget.width * ratio,
-    //     top: 20,
-    //     left: 20,
-    //     lockScalingX: true,
-    //     lockScalingY: true,
+
     //   });
     // canvas.add(imgInstance);
     // canvas.renderAll();
-
-
-      // save the canvas to JSON/serialize
-  // saveFloorplan(event) {
-  //   event.preventDefault()
-  //   console.log('submitted')
-  //   // set all objects as transparent and unselectable
-  //   this.canvas.forEachObject( o => {
-  //     o.set({
-  //       selectable: false,
-  //       opacity: 0
-  //     })
-  //   })
-
-  //   const floorplan = JSON.stringify(this.canvas)
-  //   this.#insertFloorplan(floorplan)
-  // }
-
-  // #insertFloorplan(floorplan) {
-  //   const url = this.saveFloorplanTarget.action
-  //   this.inputTarget.value = floorplan
-  //   const formData = new FormData(this.saveFloorplanTarget)
-
-  //   console.log(formData)
-  //   fetch(url, {
-  //       method: 'PATCH',
-  //       body: formData
-  //     }
-  //   ).then(response => response.json)
-  //   .then(window.location.href=`/agency/apartments/${this.idValue}`
-  //   )
-  // }
