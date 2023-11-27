373f3b0fffdb66223f83176e478f94391affd1c7
Rina Iwamine <leddihome@gmail.com>
2023-11-28 00:08:26 +0900

    added comments to requests channel and connection

--- app/channels/requests_channel.rb
+++ app/channels/requests_channel.rb
@@ -1,7 +1,8 @@
 class RequestsChannel < ApplicationCable::Channel
   def subscribed
-    user = User.find(params[:user_id])
-    stream_for user if user.agency
+    stream_for current_user if current_user.agency
+    # user = User.find(params[:user_id])
+    # stream_for user if user.agency
   end
 
   def unsubscribed
