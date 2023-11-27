e508421073c3c6ecbe0cbf151720433221e6d79c
Rina Iwamine <leddihome@gmail.com>
2023-11-27 17:52:42 +0900

    created js controller for requests

--- app/javascript/controllers/requests_subscription_controller.js
+++ app/javascript/controllers/requests_subscription_controller.js
@@ -1,7 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="requests-subscription"
 export default class extends Controller {
+  static targets = ["userMessages", "requests"]
+  static values = {agencyId:Number}
+
   connect() {
+    console.log(this.requestsTarget)
+    console.log(this.agencyIdValue)
+    this.channel = createConsumer().subscriptions.create(
+      {channel: "RequestsChannel", user_id: this.agencyIdValue},
+      {received: data => console.log(data)}
+    )
   }
 }
