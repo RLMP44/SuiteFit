7278e392fef7b220e6564e66b109a80988546be5
Rachael Momber <rlmomber@gmail.com>
2024-01-16 12:33:14 +0900

    added items_controller_spec

---
+++ spec/controllers/items_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ItemsController, type: :controller do
+  describe '#create' do
+    it 'should save an instance of Item to the db' do
+      params = { item: { name: 'Table', length: 30, width: 60 } }
+      post :create, params: params
+      expect(response).to eq('')
+    end
+  end
+end
