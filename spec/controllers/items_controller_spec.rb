e2601d7b54c8cc5fbc761fec023e4c54b5890d17
Rachael Momber <rlmomber@gmail.com>
2024-01-17 20:14:56 +0900

    added render_views and controller helpers to rails_helper file

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
+  describe 'GET #index' do
+    it 'should display all items' do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
   describe '#create' do
     it 'should save an instance of Item to the db' do
+      user = User.create!(username: "Rachael", password: "wowowow")
       params = { item: { name: 'Table', length: 30, width: 60 } }
       post :create, params: params
       expect(Item.last.name).to eq('Table')
