8f04bd9b2556676d9dfdf98aacc5b15133cb1c08
Rachael Momber <rlmomber@gmail.com>
2024-01-23 20:47:49 +0900

    added item delete rspec tests. commented out older version, but both are working

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe ItemsController, type: :controller do
   # log user in before each one to get user-specific items
   before(:each) do
-    user = User.create!(username: "Rachael", password: "wowowow", email: "rachael@rachael.com")
-    sign_in(user)
+    @user = User.create!(username: "Rachael", password: "wowowow", email: "rachael@rachael.com")
+    sign_in(@user)
   end
 
   describe 'GET #index' do
@@ -14,11 +14,29 @@ RSpec.describe ItemsController, type: :controller do
     end
   end
 
-  describe '#create' do
+  describe 'POST #create' do
     it 'should save an instance of Item to the db' do
       params = { item: { name: 'Table', length: 30, width: 60 } }
-      expect { post :create, params: params }.to change{ Item.count }.by(1)
+      expect { post :create, params: params }.to change { Item.count }.by(1)
       expect(Item.last.name).to eq('Table')
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'should destroy the last item' do
+      item = Item.create!(name: 'Table', length: 30, width: 60, user: @user)
+      item.save
+      expect { delete :destroy, params: { id: item.id } }.to change { Item.count }.by(-1)
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    it 'should change count of items by -1' do
+      item = Item.create!(name: 'Table', length: 30, width: 60, user: @user)
+      item.save
+      original_count = Item.count
+      item.destroy
+      expect(Item.count).to eq(original_count - 1)
     end
   end
 end
